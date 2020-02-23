3073e976ee7e14ffdc18f165645faa09fa4280c9
Your Name <preshasuna@gmail.com>
2020-02-23 20:44:05 +0545

    ASsignment 2

---
+++ fullname.js
@@ -0,0 +1,8 @@
+let name = {
+    first_name : "Suyog",
+    second_name : "Pradhan"
+};
+
+let names = Object.values(name);
+
+console.log(`Full name is ${names[0]} ${names[1]}`);
---
+++ propertiesobject.js
@@ -0,0 +1,9 @@
+let car = {
+    Brand : "Mitsubishi",
+    MakeYear : 2009,
+    EngineDisplacement : "5000cc"
+};
+ 
+for(let keys in car){
+    console.log(keys);
+}
